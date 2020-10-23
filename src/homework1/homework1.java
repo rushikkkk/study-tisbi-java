3ef4a27780194f2caacc4be3a22c32b2d7a17d23
rushikkkk <psgooooops@gmail.com>
2020-10-23 18:06:31 +0300

    work4-3

--- src/homework1/homework1.java
+++ src/homework1/homework1.java
@@ -3,18 +3,10 @@ import java.util.Random;
 
 public class homework1 {
     public static void main(String[] args) {
-        Random random = new Random(); // создаем объект рандом для генерации случайных чисел
-        int [] number = new int [10]; //создаем массив number с длинною 10
-        for (int i = 0; i < 10; i++){ // используем цикл for для перебора массива number
-            number[i] = random.nextInt(10)* 5; // Генерируем число от 0 до 10 и добовляем это число в массив
-
+        for(int i = 1; i < 10; i++){
+            for(int k = 1; k < 10; k++){
+                System.out.print(k * i + "  ");
+            }
+            System.out.println("");
         }
-       // System.out.println(number[5] +"это я"); // Выводим шестой элемент массива
-        int y = 1;
-        for (int b: number){ //Перебираем содержимое массива для печати
-            System.out.println(y++ +". "+ b); // Печатаем массив
-
-        }
-
-    }
-}
+    }        }
